376fd165816ee53abef8730ea19db444ef7a36a9
Patrick Scully <patrick@possibleweb.com>
2020-10-22 14:54:32 -0400

    Create Controller and View and tie together

--- src/script.js
+++ src/script.js
@@ -31,3 +31,39 @@ function show(el) {
 }
 
 // END HELPER FUNCTIONS
+
+// The View (View)
+
+class View {
+  constructor() {
+    this.app = getEl("app");
+    this.heading = createEl("h1");
+    this.addBowler = createForm("add-bowler", "Start Game");
+    this.addShot = createForm("roll-ball", "Roll The Ball");
+    this.total = createEl("div");
+    this.total.id = "total";
+    hide(this.addShot);
+  }
+
+  buildView = () => {
+    this.heading.textContent = "Welcome to Bowling!";
+    this.app.appendChild(this.heading);
+    this.app.appendChild(this.addBowler);
+    this.app.appendChild(this.addShot);
+    this.app.appendChild(this.total);
+  };
+}
+
+// The Lane (Controller)
+
+class Lane {
+  constructor(view, string) {
+    this.view = view;
+    this.string = string;
+    this.strings = [];
+
+    this.view.buildView();
+  }
+}
+
+const app = new Lane(new View(), new String());
